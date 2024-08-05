836b174ae4b0a1eb82fb5442784eef89dc6d9d82
Jay Chew <jaychew.3753@gmail.com>
2024-08-05 15:52:59 +0800

    remove unwanted functions

--- utils/google-spreadsheet.go
+++ utils/google-spreadsheet.go
@@ -3,83 +3,15 @@ package utils
 import (
 	"context"
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
-
-func GetGoogleSpreadSheetConfig() *oauth2.Config {
-
-	b, err := os.ReadFile("form-db-gs-dce6cdecba59.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
-
-	config, err := google.ConfigFromJSON(b, sheets.SpreadsheetsReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-
-	return config;
-}
-
-func SaveGoogleSpreadsheetToken(token *oauth2.Token) {
-	path := "token.json";
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func ReplaceGoogleSpreadsheetToken(authCode string) {
-	config := GetGoogleSpreadSheetConfig();
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-
-	SaveGoogleSpreadsheetToken(tok)
-	fmt.Println("Token successfully replaced and saved.")
-}
-
-func GetGoogleSpreadsheetTokenFromFile() (*oauth2.Token, error) {
-	f, err := os.Open("token.json")
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-func GetGoogleSpreadsheetTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
 
 func GoogleSpreadSheetSRV() *sheets.Service {
 	ctx := context.Background()
@@ -170,22 +102,6 @@ func GetGoogleSpreadSheetIdByName(srv *sheets.Service, sheetName string, spreads
 
 	// Return an error if no matching sheet is found
 	return -1, fmt.Errorf("no sheet found with name: %s", sheetName)
-}
-
-func CreateNewGoogleSpreadSheet(srv *sheets.Service, sheetName string) string {
-	spreadsheet := &sheets.Spreadsheet{
-    Properties: &sheets.SpreadsheetProperties{
-      Title: sheetName,
-    },
-	}
-
-	spreadsheet, err := srv.Spreadsheets.Create(spreadsheet).Do()
-	if err != nil {
-		log.Fatalf("Unable to create spreadsheet. %v", err)
-	}
-	
-	fmt.Printf("Spreadsheet ID: %s\n", spreadsheet.SpreadsheetId)
-	return spreadsheet.SpreadsheetId
 }
 
 func ClearGoogleSpreadSheet(srv *sheets.Service, sheetName string, spreadsheetId string) error {
